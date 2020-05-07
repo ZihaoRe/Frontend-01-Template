87628c9c7bae1587319e091e5a2eb3f9b33758f5
ZihaoRe <reus@foxmail.com>
2020-05-07 23:01:48 +0800

    week4 realm

---
+++ week05/realm.js
@@ -0,0 +1,98 @@
+let set = new Set();
+let globalProperties = [
+    "eval",
+    "isFinite",
+    "isNaN",
+    "parseFloat",
+    "parseInt",
+    "decodeURI",
+    "decodeURIComponent",
+    "encodeURI",
+    "encodeURIComponent",
+    "Array",
+    "Date",
+    "RegExp",
+    "Promise",
+    "Proxy",
+    "Map",
+    "WeakMap",
+    "Set",
+    "WeakSet",
+    "Function",
+    "Boolean",
+    "String",
+    "Number",
+    "Symbol",
+    "Object",
+    "Error",
+    "EvalError",
+    "RangeError",
+    "ReferenceError",
+    "SyntaxError",
+    "TypeError",
+    "URIError",
+    "ArrayBuffer",
+    "SharedArrayBuffer",
+    "DataView",
+    "Float32Array",
+    "Float64Array",
+    "Int8Array",
+    "Int16Array",
+    "Int32Array",
+    "Uint8Array",
+    "Uint16Array",
+    "Uint32Array",
+    "Uint8ClampedArray",
+    "Atomics",
+    "JSON",
+    "Math",
+    "Reflect"
+];
+let queue = [];
+for (let i=0;i<globalProperties.length;i++) {
+    queue.push({
+        path: [globalProperties[i]],
+        object: this[globalProperties[i]]
+    });
+}
+let current
+while (queue.length) {
+    current = queue.shift();
+    console.log(current.path.join("."));
+    if (set.has(current.object))
+        continue;
+    set.add(current.object);
+
+    //console.log(current)
+
+    let proto = Object.getPrototypeOf(current.object);
+    if (proto) {
+        queue.push({
+            path: current.path.concat(["__proto__"]),
+            object: proto
+        })
+    }
+
+    for (let p of Object.getOwnPropertyNames(current.object)) {
+        let property = Object.getOwnPropertyDescriptor(current.object, p);
+        if (property.hasOwnProperty("value") && ((property.value != null) && (typeof property.value == "object"))
+            && property.value instanceof Object) {
+            queue.push({
+                path: current.path.concat([p]),
+                object: property.value
+            })
+        }
+        if (property.hasOwnProperty("get") && typeof property.get == "function") {
+            queue.push({
+                path: current.path.concat([p]),
+                object: property.get
+            })
+        }
+        if (property.hasOwnProperty("set") && (typeof property.set == "function")) {
+            queue.push({
+                path: current.path.concat([p]),
+                object: property.set
+            })
+        }
+    }
+}
