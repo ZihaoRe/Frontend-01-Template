7df05211a97200c966ea9ccd8a7f1eff889f3096
ZihaoRe <reus@foxmail.com>
2020-08-08 20:10:04 +0800

    test

---
+++ week18/tools/run-vue.js
@@ -0,0 +1,7 @@
+const compiler = require("@vue/compiler-sfc");
+
+let output = compiler.compileTemplate({
+    filename: 'example.vue',
+    source: "<div>Hello World!</div>"
+})
+console.log(output)
